0246c9c48f4c41d084a59943a7309c4068a27125
Pedro Henrique de Carli Martins <pddecarli@hotmail.com>
2021-06-11 19:15:07 -0400

    primeiro commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'aade27c6d4e577cf443e40d166fcfec1'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '3b93e4e4-1f71-4480-9098-f22cc527d25d'
+};
